df2798b03929fd5ae1adfb35e11d4c322b80fbeb
deepali17 <deepali30vits@gmail.com>
2021-04-30 22:25:21 +0530

    filtered expense list as per selected year using filter method

--- src/Components/Expenses/Expense.js
+++ src/Components/Expenses/Expense.js
@@ -6,17 +6,24 @@ import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState("2020");
+  const [expenseList, filteredExpenseList] = useState(props.items);
+
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
+
+  const filtered = expenseList.filter(e => {
+    return e.date.getFullYear().toString() === filteredYear;
+  });
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onchangeFilter={filterChangeHandler}
       />
-      {props.items.map(expense => (
+      {filtered.map(expense => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
